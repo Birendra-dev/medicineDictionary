6a8fb7d43e09018fcd1028188b287e752fb09c8d
Birendra <077bct025@ioepc.edu.np>
2024-01-22 20:35:27 +0545

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,redirect,request
+
+app = Flask(__name__)
+
+@app.route("/",methods=['GET','POST'])
+def index():
+    message = ''
+    if request.method == 'POST':
+        name_of_medicine = request.form['medicine']
+        message = name_of_medicine
+    else:
+        name_of_medicine = ''  # Provide a default value if the request method is not 'POST'
+        
+    return render_template('index.html', message=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    
